4a92737908d3e295feb9923b99476ebac1a0255b
Saeed Aliyu <saeedaliyu@Saeeds-MacBook-Pro.local>
2015-08-04 21:52:17 +0100

    Initial Commit

---
+++ Lumberjack_sequencing/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Lumberjack_sequencing
+//
+//  Created by Saeed Aliyu on 8/4/15.
+//  Copyright (c) 2015 Saeed Aliyu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
